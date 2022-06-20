c45ce38f3726315e359627a6563ebf5e4f36059b
yayuan_cassie <84106763+falicitt@users.noreply.github.com>
2022-06-21 11:09:31 +1200

    add usestate and useeffect

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+ 
+
 
 function App() {
+  const [Widgets, setWidgets] = useState([])
+
+  useEffect(() => {
+    console.log('using the effect')
+  }, [])
+  
   return (
     <div>
       <h1>Widgets for the win!</h1>
